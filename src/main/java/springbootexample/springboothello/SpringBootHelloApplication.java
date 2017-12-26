81966a9e2a86c4448ea52c7deed5ad8329a52e76
937172602@qq.com <937172602@qq.com>
2017-12-26 16:18:01 +0800

    second commit

--- src/main/java/springbootexample/springboothello/SpringBootHelloApplication.java
+++ src/main/java/springbootexample/springboothello/SpringBootHelloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloApplication {
 
 	public static void main(String[] args) {
+		System.out.println("???");
 		SpringApplication.run(SpringBootHelloApplication.class, args);
 	}
 }
